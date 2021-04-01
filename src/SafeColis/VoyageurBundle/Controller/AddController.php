c9270064444e3d7292508f843ff147f8b88b24a5
jwphantom <44881349+jwphantom@users.noreply.github.com>
2021-04-01 21:09:13 +0100

    fix price abonnement

--- src/SafeColis/VoyageurBundle/Controller/AddController.php
+++ src/SafeColis/VoyageurBundle/Controller/AddController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace SafeColis\VoyageurBundle\Controller;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
@@ -12,7 +13,8 @@ use SafeColis\VoyageurBundle\Entity\Voyageur;
 //form
 use SafeColis\VoyageurBundle\Form\VoyageurType;
 //api cloudinary
-use Cloudinary\Uploader;
+use Cloudinary\Configuration\Configuration;
+use Cloudinary\Api\Upload\UploadApi;
 
 //Ajout d'un voyage dans la bd
 class AddController extends Controller
@@ -24,7 +26,7 @@ class AddController extends Controller
     private $user;
     //variable d'enregistrement du voyage
     private $voyageur;
-    
+
     public function addAction(Request $request, $mode)
     {
 
@@ -38,8 +40,7 @@ class AddController extends Controller
         $test = $currenttimestamp->format('U');
 
 
-        if(!isset($abonnement) || $abonnement->getActive() == false || $abonnement->getDateFin() < $test)
-        {
+        if (!isset($abonnement) || $abonnement->getActive() == false || $abonnement->getDateFin() < $test) {
 
             $request->getSession()->getFlashBag()->add('not_found_subscription', 'Vous n\'êtes pas encore abonné.');
             return $this->redirectToRoute('safe_colis_abonnement_status');
@@ -53,24 +54,23 @@ class AddController extends Controller
 
         /* si l'utilisateur n'a pas encore completer ses informations sur son profile on le 
         redirige pour completion*/
-        if(!$user->getNom() && !$user->getPrenom())
-        {
+        if (!$user->getNom() && !$user->getPrenom()) {
             $request->getSession()->getFlashBag()->add('completer_son_profil', 'Veuillez Renseigner votre nom et prenom pour pouvoir ajouter un voyage');
             return $this->redirectToRoute('profile_view');
         }
 
         //instancier la classe voyageur pour enregistrement des informations de voyage
-        $voyageur = new Voyageur();  
-        
+        $voyageur = new Voyageur();
+
         //si le formulaire est remplie et validé on enregistre les informations
         $form   = $this->get('form.factory')->create(VoyageurType::class, $voyageur);
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid())  {
-            
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+
             $heureDepart = $_POST['heuredepart'];
             $dateDepart = $_POST['dateDepart'];
             $kilodispo = $_POST['kilodispo'];
             $prixkilo = $_POST['prixkilo'];
-                    
+
             $voyageur->setUser($user);
             $voyageur->setHeuredepart($heureDepart);
             $voyageur->setKilodisponible($kilodispo);
@@ -79,86 +79,88 @@ class AddController extends Controller
             $voyageur->setActive(0);
             $voyageur->setKilovendu(0);
             $voyageur->setDatedepart($dateDepart);
-            $em = $this->getDoctrine()->getManager(); 
+
+            $em = $this->getDoctrine()->getManager();
             $em->persist($voyageur);
             $em->flush();
 
-            $pathtransporteur = $em->getRepository('SafeColisVoyageurBundle:Voyageur')->find($voyageur);     
+            $pathtransporteur = $em->getRepository('SafeColisVoyageurBundle:Voyageur')->find($voyageur);
             $identificationpath = $voyageur->getIdentification()->getwebPath();
             $justificationpath = $voyageur->getJustification()->getwebPath();
             $idvoyageur = $user->getId();
 
-            \Cloudinary::config(array( 
-                "cloud_name" => "doxl9x2a0", 
-                "api_key" => "439711648162288", 
-                "api_secret" => "J9xXs3_uer-x7SKng_BExnrqZ-8", 
-                "secure" => true
-              ));
+            Configuration::instance([
+                'cloud' => [
+                    'cloud_name' => 'doxl9x2a0',
+                    'api_key' => '439711648162288',
+                    'api_secret' => 'J9xXs3_uer-x7SKng_BExnrqZ-8'
+                ],
+                'url' => [
+                    'secure' => true
+                ]
+            ]);
 
-            if($mode == "aerien")
-            {
-                // on enregistre la piece d'identification dans cloudinary
-                \Cloudinary\Uploader::upload($identificationpath, array(
-                    "folder" => 'Voyageur/'.$idvoyageur."/passport/",
-                    "public_id" => $voyageur->getIdentification()->getId()
-                    ));
 
-                // on enregistre la piece de justififcation de voyage dans cloudinary
-                \Cloudinary\Uploader::upload($justificationpath, array(
-                    "folder" => 'Voyageur/'.$idvoyageur."/billet_avion/",
-                    "public_id" => $voyageur->getJustification()->getId()
-                    ));
+            if ($mode == "aerien") {
+                (new UploadApi())->upload($identificationpath, [
+                    'folder' => 'Voyageur/' . $idvoyageur . "/passport/",
+                    'public_id' => $voyageur->getIdentification()->getId()
+                ]);
 
-                $pathtransporteur->setUrlIdentification("Voyageur/".$idvoyageur."/passport/". $voyageur->getIdentification()->getId());
-                $pathtransporteur->setUrlJustification("Voyageur/".$idvoyageur."/billet_avion/". $voyageur->getJustification()->getId());
+
+                (new UploadApi())->upload($justificationpath, [
+                    'folder' => 'Voyageur/' . $idvoyageur . "/billet_avion/",
+                    'public_id' => $voyageur->getJustification()->getId(),
+                ]);
+
+                $pathtransporteur->setUrlIdentification("Voyageur/" . $idvoyageur . "/passport/" . $voyageur->getIdentification()->getId());
+                $pathtransporteur->setUrlJustification("Voyageur/" . $idvoyageur . "/billet_avion/" . $voyageur->getJustification()->getId());
                 $em->persist($pathtransporteur);
                 $em->flush();
-            }
-            elseif($mode == "routier"){
-                // on enregistre la piece d'identification dans cloudinary
-                \Cloudinary\Uploader::upload($identificationpath, array(
-                    "folder" => 'Voyageur/'.$idvoyageur."/carte_identité/",
-                    "public_id" => $voyageur->getIdentification()->getId()
-                    ));
+            } elseif ($mode == "routier") {
 
-                // on enregistre la piece de justififcation de voyage dans cloudinary
-                \Cloudinary\Uploader::upload($justificationpath, array(
-                    "folder" => 'Voyageur/'.$idvoyageur."/permis_conduit/",
-                    "public_id" => $voyageur->getJustification()->getId()
-                    ));
 
-                $pathtransporteur->setUrlIdentification("Voyageur/".$idvoyageur."/carte_identité/". $voyageur->getIdentification()->getId());
-                $pathtransporteur->setUrlJustification("Voyageur/".$idvoyageur."/permis_conduit/". $voyageur->getJustification()->getId());
+                (new UploadApi())->upload($justificationpath, [
+                    'folder' => 'Voyageur/' . $idvoyageur . "/carte_identité/",
+                    'public_id' => $voyageur->getIdentification()->getId(),
+                ]);
+
+                (new UploadApi())->upload($justificationpath, [
+                    'folder' => 'Voyageur/' . $idvoyageur . "/permis_conduit/",
+                    'public_id' => $voyageur->getJustification()->getId(),
+                ]);
+
+                $pathtransporteur->setUrlIdentification("Voyageur/" . $idvoyageur . "/carte_identité/" . $voyageur->getIdentification()->getId());
+                $pathtransporteur->setUrlJustification("Voyageur/" . $idvoyageur . "/permis_conduit/" . $voyageur->getJustification()->getId());
                 $em->persist($pathtransporteur);
                 $em->flush();
             }
             else{
-                // on enregistre la piece d'identification dans cloudinary
-                \Cloudinary\Uploader::upload($identificationpath, array(
-                    "folder" => 'Voyageur/'.$idvoyageur."/carte_identité/",
-                    "public_id" => $voyageur->getIdentification()->getId()
-                    ));
 
-                // on enregistre la piece de justififcation de voyage dans cloudinary
-                \Cloudinary\Uploader::upload($justificationpath, array(
-                    "folder" => 'Voyageur/'.$idvoyageur."/ticket_transport/",
-                    "public_id" => $voyageur->getJustification()->getId()
-                    ));
-                   
+                (new UploadApi())->upload($justificationpath, [
+                    'folder' => 'Voyageur/'.$idvoyageur."/carte_identité/",
+                    'public_id' => $voyageur->getIdentification()->getId(),
+                ]);
+
+                (new UploadApi())->upload($justificationpath, [
+                    'folder' => 'Voyageur/'.$idvoyageur."/ticket_transport/",
+                    'public_id' => $voyageur->getJustification()->getId(),
+                ]);
+
                 $pathtransporteur->setUrlIdentification("Voyageur/".$idvoyageur."/carte_identité/". $voyageur->getIdentification()->getId());
                 $pathtransporteur->setUrlJustification("Voyageur/".$idvoyageur."/ticket_transport/". $voyageur->getJustification()->getId());
                 $em->persist($pathtransporteur);
                 $em->flush();
             }
-           
+
 
             $request->getSession()->getFlashBag()->add('ajout-voyage', 'Examination de l\'annonce par la Direction.');
             return $this->redirectToRoute('safe_colis_voyageur_choice_transport');
         }
 
         return $this->render('SafeColisVoyageurBundle:add:add.html.twig', array(
-            'namepage'=>$namepage,
-            'mode'=>$mode,
+            'namepage' => $namepage,
+            'mode' => $mode,
             'form' => $form->createView()
         ));
     }
